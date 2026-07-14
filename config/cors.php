456d87f72fead3fb43f3a2b29b974c11f838dd33
ShuvoFifthElement <sh.das@thefifthelement.co>
2026-07-15 03:26:31 +0600

    cors policy added

--- config/cors.php
+++ config/cors.php
@@ -1,1 +1,21 @@
-'allowed_origins' => ['https://6a56a6f0f20c06404fadc056--buddyscriptsocial.netlify.app'],
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://6a56a6f0f20c06404fadc056--buddyscriptsocial.netlify.app'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
